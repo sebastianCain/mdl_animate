311c91d2f021ea2842f7c862a2dac41136ca2de7
Sebastian Cain <sebastian.cain.stuy@gmail.com>
2017-05-25 11:51:21 -0400

    everything fully working

--- script.py
+++ script.py
@@ -23,12 +23,13 @@ def run(filename):
     ident(tmp)
     stack = [ [x[:] for x in tmp] ]
     screen = new_screen()
-    tmp = []
+    tmp = new_matrix()
+    ident(tmp)
     step = 0.1
 
     basename = "anim"
     frames = 1
-    vdict = {}
+    vdict = []
 
     for i in range(len(commands)):
         a = commands[i]
@@ -44,31 +45,38 @@ def run(filename):
             frames = args[0]
 
         elif cmd == "vary":
-            vdict[args[0]] = args[1:]
-    print vdict
-
+            vdict.append(args)
 
     #generate frametable                                                                                                                                                                                                                      
     ftable = []
 
     for j in range(frames):
         fdict = {}
-        for k in vdict.keys():
-            vals = vdict[k]
+        for vals in vdict:
+            knobname = vals[0]
+            vals = vals[1:]
             if vals[0] <= j and vals[1] >= j:
                 percentcomplete = float(j - vals[0]) / float(vals[1] - vals[0])
                 interpolate = vals[2] + float(vals[3] - vals[2]) * percentcomplete
-                fdict[k] = interpolate
+                fdict[knobname] = interpolate
         ftable.append(fdict)
-
+    #print "FTABLE: " + str(ftable)
     #main drawing loop         
+
     for frame in range(frames):
         
         fd = ftable[frame]
-
+        #print "FD " + str(frame)
+        print fd
+        tmp = new_matrix()
+        ident(tmp)
+        stack = [] 
+        stack.append([ x[:] for x in tmp ])
+        print "STACK BEFORE FOR:"
+        print stack
 
         for command in commands:
-            print command
+            #print command
             c = command[0]
             args = command[1:]
 
@@ -80,10 +88,17 @@ def run(filename):
                 draw_polygons(tmp, screen, color)
                 tmp = []
             elif c == 'sphere':
+ #               print "SPHEREARGS:"
+ #               print args
                 add_sphere(tmp,
                            args[0], args[1], args[2], args[3], step)
+                print "TOP OF STACK:"
+                print stack[-1]
                 matrix_mult( stack[-1], tmp )
                 draw_polygons(tmp, screen, color)
+                print "TMP:"
+                print tmp
+                #print screen
                 tmp = []
             elif c == 'torus':
                 add_torus(tmp,
@@ -95,14 +110,19 @@ def run(filename):
                 x = 1
                 if args[-1] in fd.keys():
                     x = fd[args[-1]]
+                    print "MOVEKNOB:"
+                    print x
                 tmp = make_translate(args[0]*x, args[1]*x, args[2]*x)
                 matrix_mult(stack[-1], tmp)
                 stack[-1] = [x[:] for x in tmp]
                 tmp = []
+
             elif c == 'scale':
                 x = 1
                 if args[-1] in fd.keys():
                     x = fd[args[-1]]
+                    print "SCALEKNOB:"
+                    print x
                 tmp = make_scale(args[0]*x, args[1]*x, args[2]*x)
                 matrix_mult(stack[-1], tmp)
                 stack[-1] = [x[:] for x in tmp]
@@ -111,18 +131,23 @@ def run(filename):
                 x = 1
                 if args[-1] in fd.keys():
                     x = fd[args[-1]]
+                    #print "X " + str(x)
+                    print "ROTATEKNOB:"
+                    print x
                 theta = args[1]*x * (math.pi/180)
                 if args[0] == 'x':
-                    tmp = make_rotX(theta)
+                   tmp = make_rotX(theta)
                 elif args[0] == 'y':
                     tmp = make_rotY(theta)
                 else:
                     tmp = make_rotZ(theta)
-                    matrix_mult( stack[-1], tmp )
-                    stack[-1] = [ x[:] for x in tmp]
-                    tmp = []
+                matrix_mult( stack[-1], tmp )
+                stack[-1] = [ x[:] for x in tmp]
+                tmp = []
             elif c == 'push':
-                stack.append([x[:] for x in stack[-1]] )
+                print "PUSHSTACK:"
+                print stack[-1]
+                stack.append([l[:] for l in stack[-1]] )
             elif c == 'pop':
                 stack.pop()
             elif c == 'display':
@@ -130,4 +155,7 @@ def run(filename):
             elif c == 'save':
                 save_extension(screen, args[0])
         save_extension(screen, "anim/%s%03d.png" % (basename,frame))
+        clear_screen(screen)
+#        print "SCREEN"
+#        print screen
     os.system('convert anim/%s*.png %s.gif' % (basename,basename))
